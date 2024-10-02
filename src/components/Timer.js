cb44c59bcb7da2bdeb9674169d5a744100dc7575
Sezer <limanlarsezer@gmail.com>
2024-10-02 20:19:52 +0300

    timer

---
+++ src/components/Timer.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const secs = secondsRemaining % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+
+    [dispatch]
+  );
+
+  return <div className="timer">{mins.toString().padStart(2,'0')}:{secs.toString().padStart(2,'0')}</div>;
+}
